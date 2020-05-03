180b3a7da9d56d9c89433b554a84464306681d40
Archimedes Trajano <archimedes.trajano@devhaus.com>
2020-05-03 01:38:59 -0400

    secure the authorization endpoint so only the one realm is expose

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -6,63 +6,66 @@ import * as Keycloak from 'keycloak-js'
 Vue.config.productionTip = false
 
 const options = {
-  isEnabled: true,
-  logLevel : Vue.config.productionTip  ? 'error' : 'debug',
-  stringifyArguments : false,
-  showLogLevel : true,
-  showMethodName : true,
-  separator: '|',
-  showConsoleColors: true
+    isEnabled: true,
+    logLevel: Vue.config.productionTip ? 'error' : 'debug',
+    stringifyArguments: false,
+    showLogLevel: true,
+    showMethodName: true,
+    separator: '|',
+    showConsoleColors: true
 };
 Vue.use(VueLogger, options);
 
 
 //keycloak init options
 let initOptions = {
-  url: 'https://i.trajano.net/v1/auth', realm: 'spring-cloud-demo', clientId: 'vue-test-app', onLoad:'login-required'
-  // url: 'http://localhost:8081/auth', realm: 'keycloak-demo', clientId: 'vue-test-app', onLoad:'login-required'
+    url: 'https://i.trajano.net/v1/auth', realm: 'spring-cloud-demo', clientId: 'vue-test-app', onLoad: 'login-required'
+    // url: 'http://localhost:8081/auth', realm: 'keycloak-demo', clientId: 'vue-test-app', onLoad:'login-required'
 }
 
 let keycloak = Keycloak(initOptions);
 
-keycloak.init({ onLoad: initOptions.onLoad }).success((auth) =>{
-    
-    if(!auth) {
-      window.location.reload();
+keycloak.init({ onLoad: initOptions.onLoad }).success((auth) => {
+
+    if (!auth) {
+        window.location.reload();
     } else {
-      Vue.$log.info("Authenticated");
+        Vue.$log.info("Authenticated");
     }
 
-    keycloak.loadUserInfo();
-    new Vue({
-      render: h => h(App),
-      data: {
-        keycloak
-      },
-    }).$mount('#app')
-  
+    keycloak.loadUserProfile().success((profile) => {
+        new Vue({
+            render: h => h(App),
+            data: {
+                keycloak,
+                profile
+            },
+        }).$mount('#app')
+
+    })
+
 
     // TODO: Maybe dont store the token in the localstore, rather use it direct from the keycloak.token object
     localStorage.setItem("vue-token", keycloak.token);
     localStorage.setItem("vue-refresh-token", keycloak.refreshToken);
 
-    setInterval(() =>{
-      keycloak.updateToken(70).success((refreshed)=>{
-        if (refreshed) {
-          Vue.$log.debug('Token refreshed');
-        } else {
-          Vue.$log.warn('Token not refreshed, valid for '
-          + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
-        }
-      }).error(()=>{
-          Vue.$log.error('Failed to refresh token');
-      });
+    setInterval(() => {
+        keycloak.updateToken(70).success((refreshed) => {
+            if (refreshed) {
+                Vue.$log.debug('Token refreshed');
+            } else {
+                Vue.$log.warn('Token not refreshed, valid for '
+                    + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+            }
+        }).error(() => {
+            Vue.$log.error('Failed to refresh token');
+        });
 
 
     }, 60000)
 
-}).error(() =>{
-  Vue.$log.error("Authenticated Failed");
+}).error(() => {
+    Vue.$log.error("Authenticated Failed");
 });
 
 
