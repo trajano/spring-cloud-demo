16cf3e2cda1aab9689dceec746b4d9722d1ab33f
Archimedes Trajano <developer@trajano.net>
2022-04-19 01:49:03 -0400

    woo it works now

--- gateway/src/main/java/net/trajano/swarm/gateway/GatewayApplication.java
+++ gateway/src/main/java/net/trajano/swarm/gateway/GatewayApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClientConfiguration;
-import org.springframework.context.annotation.Import;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
-@Import(LoadBalancerClientConfiguration.class)
+@LoadBalancerClients(defaultConfiguration = LoadBalancerClientConfiguration.class)
 public class GatewayApplication {
 
   public static void main(String[] args) {
