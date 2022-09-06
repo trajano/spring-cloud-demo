f315078d5a9112e83063af9a01ca45dbe3ec1432
Archimedes Trajano <developer@trajano.net>
2022-09-06 14:27:37 -0400

    fix tests

--- jwks-provider/src/test/java/net/trajano/swarm/jwksprovider/JwkProviderApplicationTests.java
+++ jwks-provider/src/test/java/net/trajano/swarm/jwksprovider/JwkProviderApplicationTests.java
@@ -6,8 +6,13 @@ import net.trajano.swarm.jwksprovider.database.JwksDatabasePopulator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
 
 @SpringBootTest
+@MockBean(classes={
+        JwksDatabasePopulator.class
+})
 class JwkProviderApplicationTests {
 
   @Autowired private JwksDatabasePopulator jwksPopulator;
