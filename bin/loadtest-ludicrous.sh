7c9bb6b18c467169a04f544cad1ce2e8fb90db7e
Archimedes Trajano <archimedes.trajano@iblum.co>
2022-09-26 05:44:04 +0000

    test files

--- bin/loadtest-ludicrous.sh
+++ bin/loadtest-ludicrous.sh
@@ -5,7 +5,7 @@ set -x
 # artillery run --output build/report.json --environment localhost-direct-heavy-load gateway/src/test/artillery/functional-test.yml
 # artillery run --output build/report.json --environment localhost-direct-heavy-load gateway/src/test/artillery/kill-ping.yml
 # artillery run --output build/report.json --environment localhost-direct-heavy-load gateway/src/test/artillery/happy-path.yml
-artillery run --output build/report.json --environment localhost-direct-heavy-load gateway/src/test/artillery/happy-path-no-grpc.yml
+artillery run --output build/report.json --environment localhost-direct-ludicrous-load gateway/src/test/artillery/happy-path.yml
 artillery report build/report.json
 
 #DEBUG=http artillery run --output build/ppe.json --environment api-heavy-load gateway/src/test/artillery/happy-path.yml
