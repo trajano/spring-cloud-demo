2b26df7d94ccaf2e24481402b1c0e31ce5d66ee5
Archimedes Trajano <developer@trajano.net>
2022-09-27 01:42:44 -0400

    Simplify Sample Service to not use GRPC anymore for Echo service

--- sample-service/src/main/java/net/trajano/swarm/sampleservice/echo/EchoController.java
+++ sample-service/src/main/java/net/trajano/swarm/sampleservice/echo/EchoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -21,7 +22,7 @@ public class EchoController {
       path = "/Echo/echo",
       consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.APPLICATION_JSON_VALUE)
-  public Mono<EchoResponse> echo(EchoRequest request) {
+  public Mono<EchoResponse> echo(@RequestBody EchoRequest request) {
 
     return Mono.just(
         EchoResponse.builder().message(request.getMessage()).timestamp(Instant.now()).build());
@@ -31,7 +32,7 @@ public class EchoController {
       path = "/Echo/echoStream",
       consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public Flux<ServerSentEvent<EchoResponse>> echoStream(EchoRequest request) {
+  public Flux<ServerSentEvent<EchoResponse>> echoStream(@RequestBody EchoRequest request) {
 
     return Flux.<EchoResponse>generate(
             sink -> {
