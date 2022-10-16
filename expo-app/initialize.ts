7433cd0e2a902c4ef4cace07b5bdd3b880f10774
Archimedes Trajano <developer@trajano.net>
2022-10-16 08:53:09 -0400

    added missing file

---
+++ expo-app/initialize.ts
@@ -0,0 +1,9 @@
+global.Buffer = require("buffer").Buffer;
+import { polyfillWebCrypto } from "expo-standard-web-crypto";
+polyfillWebCrypto();
+const TextEncodingPolyfill = require("text-encoding");
+
+Object.assign(global, {
+  TextEncoder: TextEncodingPolyfill.TextEncoder,
+  TextDecoder: TextEncodingPolyfill.TextDecoder,
+});
