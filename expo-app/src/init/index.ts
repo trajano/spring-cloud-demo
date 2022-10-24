342a075ecbb68ec3dc95c3aef56fd42546c4619e
Archimedes Trajano <developer@trajano.net>
2022-10-24 15:26:59 -0400

    Relocate app entry point

--- expo-app/src/init/index.ts
+++ expo-app/src/init/index.ts
@@ -1,4 +1,9 @@
+/**
+ * This is the app entrypoint where the root component is registered and polyfills (if any) are set up.
+ */
 import { registerRootComponent } from "expo";
+
+import './initialize';
+
 import App from "./App";
-console.log("Hello");
 export default registerRootComponent(App);
