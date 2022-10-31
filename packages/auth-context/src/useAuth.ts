39eca4dddef8079ba36ae3355804ce236087f7ae
Archimedes Trajano <developer@trajano.net>
2022-10-31 01:45:22 -0400

    Removed non-relevant files

--- packages/auth-context/src/useAuth.ts
+++ packages/auth-context/src/useAuth.ts
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "./AuthContext";
-import { IAuth } from "./IAuth";
+import type { IAuth } from "./IAuth";
 
 export function useAuth(): IAuth {
   return useContext(AuthContext);
