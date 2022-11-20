846357f5e9cf79d67d0f4c20635973e7985f9e07
Archimedes Trajano <archimedes.trajano@devhaus.com>
2022-11-20 12:08:15 -0500

    revert redux

--- expo-app/authenticated-context/AuthenticatedProvider.tsx
+++ expo-app/authenticated-context/AuthenticatedProvider.tsx
@@ -1,29 +1,16 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { useAsyncSetEffect, useMounted } from "@trajano/react-hooks";
 import { useAuth } from "@trajano/spring-docker-auth-context";
 import { PropsWithChildren, useEffect, useMemo, useReducer, useRef, useState } from "react";
 import EventSource from "react-native-sse";
-import { Provider } from "react-redux";
 import { AuthenticatedContext } from "./IAuthenticatedContext";
 import { JwtClaims } from "./JwtClaims";
 import { jwtVerify } from "./jwtVerify";
-import * as reducers from "./reducers";
-import { persistStore, persistReducer } from 'redux-persist'
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { PersistGate } from 'redux-persist/integration/react';
-import { AUTHENTICATED, SSE_EVENT } from './actions';
-import Constants from 'expo-constants';
 
 // At present this has a problem on restore in that the access token is not valid yet.
 type AuthenticatedProviderProps = PropsWithChildren<{
     clientId: string,
     issuer: string
 }>;
-/**
- * This provider manages elements that are associated with the current login.  Including the Redux store.
- * @param param0 
- * @returns 
- */
 export function AuthenticatedProvider({ clientId, issuer, children }: AuthenticatedProviderProps) {
 
     const { baseUrl, accessToken } = useAuth();
@@ -34,19 +21,7 @@ export function AuthenticatedProvider({ clientId, issuer, children }: Authentica
 
     const eventStream = useRef<EventSource<string>>();
 
-    const [internalState, setInternalState] = useState([]);
-
-    const persistConfig = useMemo(() => {
-        const prefix = (Constants.manifest2?.id ?? Constants.manifest?.originalFullName);
-        return ({ key: verified ? `${prefix}:${username}` : `${prefix}:unknown`, storage: AsyncStorage });
-    }, [username]);
-    const persistedReducer = useMemo(() => persistReducer(persistConfig, combineReducers(reducers)), [persistConfig, reducers]);
-    const store = useMemo(() => configureStore({
-        reducer: persistedReducer,
-        middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
-    }), [persistedReducer]);
-    const persistor = useMemo(() => persistStore(store), [store]);
-
+    const [internalState, updateInternalStateFromServerSentEvent] = useReducer((state: string[], nextEvent: string) => { return [...state, nextEvent].slice(-5) }, [])
     useAsyncSetEffect(async function verifyToken() {
         // when access token changes the value could fail.
         // when the internet is broken then the verification will fail 
@@ -77,7 +52,7 @@ export function AuthenticatedProvider({ clientId, issuer, children }: Authentica
             });
             eventStream.current.addEventListener("message", (event) => {
                 if (event.type === "message" && event.data && isMounted()) {
-                    store.dispatch({ type: SSE_EVENT, payload: event.data })
+                    updateInternalStateFromServerSentEvent(event.data);
                 }
             })
             return () => eventStream.current?.close();
@@ -100,19 +75,6 @@ export function AuthenticatedProvider({ clientId, issuer, children }: Authentica
 
     }
 
-    useEffect(() => {
-        store.dispatch({ type: AUTHENTICATED, payload: claims });
-        return store.subscribe(() => {
-            setInternalState(store.getState().sseState)
-        })
-    }, [claims]);
-
-    useEffect(() => {
-        if (process.env.NODE_ENV !== 'production' && module.hot) {
-            module.hot.accept('./reducers', () => store.replaceReducer(persistReducer(persistConfig, combineReducers(reducers))));
-        }
-    }, []);
-
     return (<AuthenticatedContext.Provider value={{
         internalState,
         username,
@@ -120,10 +82,6 @@ export function AuthenticatedProvider({ clientId, issuer, children }: Authentica
         whoami,
         claims
     }}>
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                {children}
-            </PersistGate>
-        </Provider>
+        {children}
     </AuthenticatedContext.Provider>);
 }
