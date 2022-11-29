c4d77fbc8bedf47ee8461b70b9d949026e1356a2
Archimedes Trajano <developer@trajano.net>
2022-11-29 02:22:57 -0500

    Fix compile error

--- packages/auth-context/example/src/App.tsx
+++ packages/auth-context/example/src/App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from '@trajano/spring-docker-auth-context';
+import { AuthState } from '@trajano/spring-docker-auth-context';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    Promise.resolve(AuthState.AUTHENTICATED).then(setResult);
   }, []);
 
   return (
