a0baa0f697b5b3207a934ef8395bd15670e9f3c4
Archimedes Trajano <developer@trajano.net>
2022-12-01 04:23:27 -0500

    X

--- expo-app/screens/TabTwoScreen.tsx
+++ expo-app/screens/TabTwoScreen.tsx
@@ -3,6 +3,7 @@ import { AuthState, useAuth } from '@trajano/spring-docker-auth-context';
 import { format, Locale } from 'date-fns';
 import * as dateFnsLocales from 'date-fns/locale';
 import * as Localization from 'expo-localization';
+import { useReducer } from 'react';
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { Button, Keyboard, NativeScrollEvent, NativeSyntheticEvent, ScrollView as RNScrollView, StyleSheet, TextInputFocusEventData } from 'react-native';
 import { ScrollView, Text, TextInput, View } from '../src/components';
@@ -27,14 +28,23 @@ export default function TabTwoScreen() {
   }, [])
   const [now, setNow] = useState(format(Date.now(), "PPpp", { locale }))
   const [username, setUsername] = useState("")
+  type KeyboardEventType = {
+
+  }
+  const [keyboardLog, logKeyboardEvent] = useReducer(
+    (prevState: { key: string, name: string, event: any }[], action: { eventName: string, event?: any }) => action.eventName === "clear" ? [] : [...prevState, {
+      name: action.eventName,
+      event: action.event,
+      key: action.eventName + "-" + Date.now()
+    }], []);
 
   useEffect(() => {
-    const keyboardWillShowSubscription = Keyboard.addListener("keyboardWillShow", (e) => { console.log({ keyboardWillShow: e }) });
-    const keyboardDidShowSubscription = Keyboard.addListener("keyboardDidShow", (e) => { console.log({ keyboardDidShow: e }) });
-    const keyboardWillHideSubscription = Keyboard.addListener("keyboardWillHide", (e) => { console.log({ keyboardWillHide: e }) });
-    const keyboardDidHideSubscription = Keyboard.addListener("keyboardDidHide", (e) => { console.log({ keyboardDidHide: e }) });
-    const keyboardWillChangeFrameSubscription = Keyboard.addListener("keyboardWillChangeFrame", (e) => { console.log({ keyboardWillChangeFrame: e }) });
-    const keyboardDidChangeFrameSubscription = Keyboard.addListener("keyboardDidChangeFrame", (e) => { console.log({ keyboardDidChangeFrame: e }) });
+    const keyboardWillShowSubscription = Keyboard.addListener("keyboardWillShow", (e) => { logKeyboardEvent({ eventName: "keyboardWillShow", event: e }) });
+    const keyboardDidShowSubscription = Keyboard.addListener("keyboardDidShow", (e) => { logKeyboardEvent({ eventName: "keyboardDidShow", event: e }) });
+    const keyboardWillHideSubscription = Keyboard.addListener("keyboardWillHide", (e) => { logKeyboardEvent({ eventName: "keyboardWillHide", event: e }) });
+    const keyboardDidHideSubscription = Keyboard.addListener("keyboardDidHide", (e) => { logKeyboardEvent({ eventName: "keyboardDidHide", event: e }) });
+    const keyboardWillChangeFrameSubscription = Keyboard.addListener("keyboardWillChangeFrame", (e) => { logKeyboardEvent({ eventName: "keyboardWillChangeFrame", event: e }) });
+    const keyboardDidChangeFrameSubscription = Keyboard.addListener("keyboardDidChangeFrame", (e) => { logKeyboardEvent({ eventName: "keyboardDidChangeFrame", event: e }) });
     return () => {
       keyboardWillShowSubscription.remove();
       keyboardDidShowSubscription.remove();
@@ -65,14 +75,13 @@ export default function TabTwoScreen() {
       <Text>Hello {username} <Text style={{ fontWeight: "bold" }}>bold</Text> <Text style={{ fontStyle: "italic" }}>italic</Text></Text>
       <TextInput placeholder='Username' defaultValue={username} onChangeText={setUsername} width={300} onFocus={onFocus} backgroundColor="#DDDDFF" />
       <Text fg="blue">{AuthState[authState]}</Text>
-      <Text>{JSON.stringify(scrollInfo)}</Text>
+      <Text>{JSON.stringify(scrollInfo, null, 2)}</Text>
       <View height={headerHeight} width={100} bg="red" fg="yellow" alignItems='center' justifyContent='center'>
         <Text fg="yellow" >{headerHeight}</Text>
       </View>
       <Button title='scroll' onPress={() => scrollViewRef.current?.scrollTo({ y: 30, animated: true })} />
-      <Text style={{ fontFamily: "IBMPlexSans", fontSize: 30 }}>IBMPlexSans <Text style={{ fontWeight: "bold" }}>bold</Text> <Text style={{ fontStyle: "italic" }}>italic</Text> {colorScheme}</Text>
-      <Text style={{ fontFamily: "Lexend", fontSize: 20 }}>Lexend has no <Text style={{ fontStyle: "italic" }}>italic <Text style={{ fontWeight: "bold" }}>bold</Text> </Text></Text>
-      <Text style={{ fontFamily: "IslandMoments", fontSize: 40 }}>IslandMoments has no <Text style={{ fontStyle: "italic" }}>italic <Text style={{ fontWeight: "bold" }}>bold</Text></Text><Text style={{ fontWeight: "bold" }}>bold</Text></Text>
+      <Button title='clear' onPress={() => logKeyboardEvent({ eventName: "clear" })} />
+      {keyboardLog.map((entry) => <Text key={entry.key}>{entry.name} <Text>{JSON.stringify(entry.event)}</Text></Text>)}
       <Text style={{ fontFamily: "IBMPlexSans", fontSize: 30 }}>IBMPlexSans <Text style={{ fontWeight: "bold" }}>bold</Text> <Text style={{ fontStyle: "italic" }}>italic</Text> {colorScheme}</Text>
       <Text style={{ fontFamily: "Lexend", fontSize: 20 }}>Lexend has no <Text style={{ fontStyle: "italic" }}>italic <Text style={{ fontWeight: "bold" }}>bold</Text> </Text></Text>
       <Text style={{ fontFamily: "IslandMoments", fontSize: 40 }}>IslandMoments has no <Text style={{ fontStyle: "italic" }}>italic <Text style={{ fontWeight: "bold" }}>bold</Text></Text><Text style={{ fontWeight: "bold" }}>bold</Text></Text>
