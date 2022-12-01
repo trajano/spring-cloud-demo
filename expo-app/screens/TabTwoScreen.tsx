6763907b83fb605010e4fdb421146b55de60c08a
Archimedes Trajano <developer@trajano.net>
2022-12-01 02:30:31 -0500

    X

--- expo-app/screens/TabTwoScreen.tsx
+++ expo-app/screens/TabTwoScreen.tsx
@@ -1,18 +1,18 @@
+import { useHeaderHeight } from '@react-navigation/elements';
+import { AuthState, useAuth } from '@trajano/spring-docker-auth-context';
 import { format, Locale } from 'date-fns';
 import * as dateFnsLocales from 'date-fns/locale';
 import * as Localization from 'expo-localization';
-import { useEffect, useMemo, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { Keyboard, NativeSyntheticEvent, ScrollView as RNScrollView, StyleSheet, TextInputFocusEventData } from 'react-native';
 import { ScrollView, Text, TextInput, View } from '../src/components';
 import { useTheming } from '../src/lib/native-unstyled';
-import { useHeaderHeight } from '@react-navigation/elements';
-import { AuthState, useAuth } from '@trajano/spring-docker-auth-context';
-import { KeyboardAvoidingView } from 'react-native';
 
 export default function TabTwoScreen() {
   const { colorScheme } = useTheming()
   const { authState } = useAuth();
   const headerHeight = useHeaderHeight();
+  const scrollViewRef = useRef<RNScrollView>();
   const locale: Locale = useMemo(() => {
 
     const dateFnsLocales2 = dateFnsLocales as Record<string, Locale>;
@@ -28,28 +28,51 @@ export default function TabTwoScreen() {
   const [username, setUsername] = useState("")
 
   useEffect(() => {
+    const keyboardWillShowSubscription = Keyboard.addListener("keyboardWillShow", (e) => { console.log({ keyboardWillShow: e }) });
+    const keyboardDidShowSubscription = Keyboard.addListener("keyboardDidShow", (e) => { console.log({ keyboardDidShow: e }) });
+    const keyboardWillHideSubscription = Keyboard.addListener("keyboardWillHide", (e) => { console.log({ keyboardWillHide: e }) });
+    const keyboardDidHideSubscription = Keyboard.addListener("keyboardDidHide", (e) => { console.log({ keyboardDidHide: e }) });
+    const keyboardWillChangeFrameSubscription = Keyboard.addListener("keyboardWillChangeFrame", (e) => { console.log({ keyboardWillChangeFrame: e }) });
+    const keyboardDidChangeFrameSubscription = Keyboard.addListener("keyboardDidChangeFrame", (e) => { console.log({ keyboardDidChangeFrame: e }) });
+    return () => {
+      keyboardWillShowSubscription.remove();
+      keyboardDidShowSubscription.remove();
+      keyboardWillHideSubscription.remove();
+      keyboardDidHideSubscription.remove();
+      keyboardWillChangeFrameSubscription.remove();
+      keyboardDidChangeFrameSubscription.remove();
+    }
+  }, [])
+
+  useEffect(() => {
 
     const c = setInterval(() => setNow(format(Date.now(), "PPpp", { locale })), 1000);
     return () => clearInterval(c);
 
   }, [locale])
+
+  const onFocus = useCallback((e: NativeSyntheticEvent<TextInputFocusEventData>) => {
+    console.log({ onFocus: e })
+    // scrollViewRef.current?.scrollTo({ y: 10, animated: true });
+  }, [])
+
   return (
-    <KeyboardAvoidingView behavior='position' style={{
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'center',
-    }} keyboardVerticalOffset={0}>
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Text bg="primary:f" fg="primary:b" style={styles.title}>{now}</Text>
-        <View style={styles.separator} />
-        <Text>Hello {username} <Text style={{ fontWeight: "bold" }}>bold</Text> <Text style={{ fontStyle: "italic" }}>italic</Text></Text>
-        <TextInput placeholder='Username' defaultValue={username} onChangeText={setUsername} width={300} backgroundColor="#DDDDFF" />
-        <Text fg="blue">{AuthState[authState]}</Text>
-        <Text style={{ fontFamily: "IBMPlexSans", fontSize: 30 }}>IBMPlexSans <Text style={{ fontWeight: "bold" }}>bold</Text> <Text style={{ fontStyle: "italic" }}>italic</Text> {colorScheme}</Text>
-        <Text style={{ fontFamily: "Lexend", fontSize: 20 }}>Lexend has no <Text style={{ fontStyle: "italic" }}>italic <Text style={{ fontWeight: "bold" }}>bold</Text> </Text></Text>
-        <Text style={{ fontFamily: "IslandMoments", fontSize: 40 }}>IslandMoments has no <Text style={{ fontStyle: "italic" }}>italic <Text style={{ fontWeight: "bold" }}>bold</Text></Text><Text style={{ fontWeight: "bold" }}>bold</Text></Text>
-      </ScrollView>
-    </KeyboardAvoidingView>
+    <ScrollView contentInsetAdjustmentBehavior="automatic" ref={scrollViewRef}>
+      <Text bg="primary:f" fg="primary:b" style={styles.title}>{now}</Text>
+      <View style={styles.separator} />
+      <Text>Hello {username} <Text style={{ fontWeight: "bold" }}>bold</Text> <Text style={{ fontStyle: "italic" }}>italic</Text></Text>
+      <TextInput placeholder='Username' defaultValue={username} onChangeText={setUsername} width={300} onFocus={onFocus} backgroundColor="#DDDDFF" />
+      <Text fg="blue">{AuthState[authState]}</Text>
+      <Text style={{ fontFamily: "IBMPlexSans", fontSize: 30 }}>IBMPlexSans <Text style={{ fontWeight: "bold" }}>bold</Text> <Text style={{ fontStyle: "italic" }}>italic</Text> {colorScheme}</Text>
+      <Text style={{ fontFamily: "Lexend", fontSize: 20 }}>Lexend has no <Text style={{ fontStyle: "italic" }}>italic <Text style={{ fontWeight: "bold" }}>bold</Text> </Text></Text>
+      <Text style={{ fontFamily: "IslandMoments", fontSize: 40 }}>IslandMoments has no <Text style={{ fontStyle: "italic" }}>italic <Text style={{ fontWeight: "bold" }}>bold</Text></Text><Text style={{ fontWeight: "bold" }}>bold</Text></Text>
+      <Text style={{ fontFamily: "IBMPlexSans", fontSize: 30 }}>IBMPlexSans <Text style={{ fontWeight: "bold" }}>bold</Text> <Text style={{ fontStyle: "italic" }}>italic</Text> {colorScheme}</Text>
+      <Text style={{ fontFamily: "Lexend", fontSize: 20 }}>Lexend has no <Text style={{ fontStyle: "italic" }}>italic <Text style={{ fontWeight: "bold" }}>bold</Text> </Text></Text>
+      <Text style={{ fontFamily: "IslandMoments", fontSize: 40 }}>IslandMoments has no <Text style={{ fontStyle: "italic" }}>italic <Text style={{ fontWeight: "bold" }}>bold</Text></Text><Text style={{ fontWeight: "bold" }}>bold</Text></Text>
+      <Text style={{ fontFamily: "IBMPlexSans", fontSize: 30 }}>IBMPlexSans <Text style={{ fontWeight: "bold" }}>bold</Text> <Text style={{ fontStyle: "italic" }}>italic</Text> {colorScheme}</Text>
+      <Text style={{ fontFamily: "Lexend", fontSize: 20 }}>Lexend has no <Text style={{ fontStyle: "italic" }}>italic <Text style={{ fontWeight: "bold" }}>bold</Text> </Text></Text>
+      <Text style={{ fontFamily: "IslandMoments", fontSize: 40 }}>IslandMoments has no <Text style={{ fontStyle: "italic" }}>italic <Text style={{ fontWeight: "bold" }}>bold</Text></Text><Text style={{ fontWeight: "bold" }}>bold</Text></Text>
+    </ScrollView>
   );
 }
 
