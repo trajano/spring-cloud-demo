050e6e6d9859cdf555e13d019d8541f6cd112343
Archimedes Trajano <developer@trajano.net>
2022-12-14 22:50:46 -0500

    Change title based on network events

--- expo-app/screens/NetworkLoggerScreen.tsx
+++ expo-app/screens/NetworkLoggerScreen.tsx
@@ -1,6 +1,19 @@
+import NetInfo, { NetInfoState, NetInfoStateType } from '@react-native-community/netinfo';
+import { useFocusEffect } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { useCallback } from 'react';
 import NetworkLogger from 'react-native-network-logger';
 
-export function NetworkLoggerScreen() {
+export function NetworkLoggerScreen({ navigation }: NativeStackScreenProps<any, "NetworkLoggerScreen">) {
+    function networkInfoHandler(nextState: NetInfoState) {
+        navigation.setOptions({
+            title: `${NetInfoStateType[nextState.type]} c=${nextState.isConnected} i=${nextState.isInternetReachable}`,
+        });
+    }
+    useFocusEffect(useCallback(() => {
+        const cancelNetInfoSubscription = NetInfo.addEventListener(networkInfoHandler);
+        return () => cancelNetInfoSubscription();
+    }, []));
     return <NetworkLogger
         theme={{
             colors: {
