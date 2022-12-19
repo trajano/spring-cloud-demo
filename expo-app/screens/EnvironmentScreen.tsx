9ebd3cf77886c6e9aedb5b5dac31812ae7bc02f5
Archimedes Trajano <developer@trajano.net>
2022-12-19 12:36:13 -0500

    Move tests to src

--- expo-app/screens/EnvironmentScreen.tsx
+++ expo-app/screens/EnvironmentScreen.tsx
@@ -12,8 +12,61 @@ import { BlurView, Text } from '../src/lib/native-unstyled';
 const isHermes = () => !!((global as any)['HermesInternal']);
 export function EnvironmentScreen(): ReactElement<SectionListProps<Record<string, unknown>>, any> {
     const { manifest, manifest2, systemFonts, expoConfig, ...restOfConstants } = Constants;
-    const [sections, setSections] = useDeepState<SectionListData<any>[]>([]);
-    async function computeSections(): Promise<SectionListData<any>[]> {
+    const [sections, setSections] = useDeepState<SectionListData<any>[]>([
+        { key: "@env", data: [{ BASE_URL, TEXT_TEST, isHermes }] },
+        {
+            key: "expo-constants", data: [
+                restOfConstants as Record<string, unknown>
+            ]
+        },
+        {
+            key: "expo-constants.expoConfig", data: [expoConfig as unknown as Record<string, unknown>]
+        },
+        {
+            key: "expo-constants.manifests", data: [
+                manifest as Record<string, unknown>,
+                manifest2 as Record<string, unknown>
+            ]
+        },
+        {
+            key: "expo-file-system",
+            data: [{
+                cacheDirectory: FileSystem.cacheDirectory,
+                documentDirectory: FileSystem.documentDirectory,
+            }]
+        },
+        {
+            key: "expo-localization.Locales",
+            data: [],
+        },
+        {
+            key: "expo-localization.Calendars",
+            data: [],
+        },
+        {
+            key: "System Fonts",
+            data: systemFonts
+                .filter(name => name.indexOf("-Bold") === -1)
+                .filter(name => name.indexOf("_Bold") === -1)
+                .filter(name => name.indexOf("_SemiBold") === -1)
+                .filter(name => name.indexOf("_Medium") === -1)
+                .filter(name => name.indexOf("-Italic") === -1)
+                .filter(name => name.indexOf("-Black") === -1)
+                .filter(name => name.indexOf("-Thin") === -1)
+                .filter(name => name.indexOf("-Semibold") === -1)
+                .filter(name => name.indexOf("-ExtraBold") === -1)
+                .filter(name => name.indexOf("-SemiBold") === -1)
+                .filter(name => name.indexOf("-Medium") === -1)
+                .filter(name => name.indexOf("-Regular") === -1)
+                .filter(name => name.indexOf("-Light") === -1)
+                .filter(name => name.indexOf("-Ultralight") === -1)
+                .filter(name => name.indexOf("-UltraBold") === -1),
+            renderItem: ({ item }: SectionListRenderItemInfo<string>) => (<View>
+                <RNText style={{ fontFamily: item, fontSize: 16, lineHeight: item === "Zapfino" ? undefined : 30 }}>{item}</RNText>
+            </View>)
+        },
+    ]);
+    async function computeSectionsWithPromises(): Promise<SectionListData<any>[]> {
         return [
             { key: "@env", data: [{ BASE_URL, TEXT_TEST, isHermes }] },
             {
@@ -71,7 +124,7 @@ export function EnvironmentScreen(): ReactElement<SectionListProps<Record<string
     }
     useFocusEffect(useCallback(() => {
         (async () => {
-            setSections(await computeSections())
+            setSections(await computeSectionsWithPromises())
         })()
     }, []));
     return <SectionList<any>
