fc08daa9bedbc7eb2d050575769169c3b9b15be3
Archimedes Trajano <developer@trajano.net>
2022-12-26 13:37:33 -0500

    Happy boxing day

--- expo-app/src/app-context/useLastAuthEvents.ts
+++ expo-app/src/app-context/useLastAuthEvents.ts
@@ -1,27 +1,34 @@
 import { useReducer } from "react";
-import type { AuthEvent } from "@trajano/spring-docker-auth-context";
+import { AuthEvent, useAuth } from "@trajano/spring-docker-auth-context";
 import { LoggedAuthEvent } from "./LoggedAuthEvent";
 
 export function useLastAuthEvents(
   logAuthEventFilterPredicate: (event: AuthEvent) => boolean,
   logAuthEventSize: number
 ) {
-  return useReducer(function lastAuthEventsReducer(
-    current: LoggedAuthEvent[],
-    nextAuthEvent: AuthEvent
-  ): LoggedAuthEvent[] {
-    if (logAuthEventFilterPredicate(nextAuthEvent)) {
-      return [
-        {
-          ...nextAuthEvent,
-          key: nextAuthEvent.type + Date.now(),
-          on: new Date(),
-        },
-        ...current,
-      ].slice(0, logAuthEventSize);
-    } else {
-      return current;
-    }
-  },
-  []);
+  const { initialAuthEvents } = useAuth();
+  return useReducer(
+    function lastAuthEventsReducer(
+      current: LoggedAuthEvent[],
+      nextAuthEvent: AuthEvent
+    ): LoggedAuthEvent[] {
+      if (logAuthEventFilterPredicate(nextAuthEvent)) {
+        return [
+          {
+            ...nextAuthEvent,
+            key: nextAuthEvent.type + Date.now(),
+            on: new Date(),
+          },
+          ...current,
+        ].slice(0, logAuthEventSize);
+      } else {
+        return current;
+      }
+    },
+    initialAuthEvents.map((event, index) => ({
+      ...event,
+      key: "initial." + index,
+      on: new Date(),
+    }))
+  );
 }
