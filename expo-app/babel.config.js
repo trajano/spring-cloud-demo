f18f9d5587351ae1f1b3356422b7ff41b84f663a
Archimedes Trajano <archimedes.trajano@devhaus.com>
2023-01-22 17:17:09 -0500

    fix: allow undefined and remove js in lint-staged

--- expo-app/babel.config.js
+++ expo-app/babel.config.js
@@ -3,7 +3,12 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "module:react-native-dotenv",
+      [
+        "module:react-native-dotenv",
+        {
+          allowUndefined: true,
+        },
+      ],
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
     ],
