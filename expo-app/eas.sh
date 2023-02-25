3b12d73ff987cf8365513cb00ab5ac2cb4708548
Archimedes Trajano <developer@trajano.net>
2023-02-24 23:45:23 -0500

    chore: update deps for Expo 48

--- expo-app/eas.sh
+++ expo-app/eas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 npm install -g eas-cli
-eas build --profile development --platform all --no-wait --non-interactive
-eas build --profile development-simulator --platform ios --no-wait --non-interactive
-eas build --profile preview --platform all --no-wait --non-interactive
+eas build --profile development --platform all --no-wait --non-interactive --clear-cache
+eas build --profile development-simulator --platform ios --no-wait --non-interactive --clear-cache
+eas build --profile preview --platform all --no-wait --non-interactive --clear-cache
 eas update --non-interactive --auto
